b9845aa82535af7b852612320d0363bd75cee063
salvoab <salvatore.abate85@gmail.com>
2020-11-17 15:45:12 +0100

    Esercizio mail completato

--- mail/assets/js/main.js
+++ mail/assets/js/main.js
@@ -4,3 +4,27 @@
  * stampa un messaggio appropriato sull’esito del controllo.
  *************************************************************/
 
+var btnVerifica = document.getElementById("btn-verifica");
+var utentiRegistrati = ["pippo@mail.com", "paperino@mail.com","pluto@mail.com"];
+
+btnVerifica.addEventListener("click", function(){
+    var message;
+    var paragrafoMessaggio = document.getElementById("messaggio");
+    var userMailInput = document.getElementById("user-mail");
+    var userMail = userMailInput.value;
+    var userTrovato = false;
+
+    for(var i=0; i<utentiRegistrati.length; i++){
+        if(userMail == utentiRegistrati[i]){
+            userTrovato=true;
+            break;
+        }
+    }
+
+    if(userTrovato){
+        paragrafoMessaggio.innerHTML = "Benvenuto utente " + userMail + " Sei un utente registrato";
+    } else{
+        paragrafoMessaggio.innerHTML = "Spiacente utente " + userMail + " Non sei un utente registrato";
+    }
+
+});
