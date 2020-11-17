e9d4e93cc43615a5619439e653c8426aa1c386df
salvoab <salvatore.abate85@gmail.com>
2020-11-17 15:24:25 +0100

    Cartelle e file per gli esercizi del giorno

---
+++ mail/assets/js/main.js
@@ -0,0 +1,6 @@
+/*************************************************************
+ * Chiedi all’utente la sua email,
+ * controlla che sia nella lista di chi può accedere,
+ * stampa un messaggio appropriato sull’esito del controllo.
+ *************************************************************/
+
